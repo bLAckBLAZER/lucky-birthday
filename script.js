edd6c422636e8c6b4a90cad5328aff2bd1fd3caf
Omkar Jadhav <48726860+bLAckBLAZER@users.noreply.github.com>
2021-08-25 23:10:26 +0530

    added content and logic

---
+++ script.js
@@ -0,0 +1,33 @@
+const birthDate = document.querySelector("#birth-date");
+const luckyNumber = document.querySelector("#lucky-number")
+var btnCheck = document.querySelector("#btn-check");
+const displayMsg = document.querySelector("#display-msg");
+
+function checkLuckyBirthday(){
+    if (birthDate.value === ""){
+        displayMsg.innerHTML = "Please enter your birth date!";
+        return
+    }
+    if (luckyNumber.value <=0){
+        displayMsg.innerHTML = "Lucky Number should be greater or equal to 1";
+        return
+    }
+
+    const birthDateStr = birthDate.value.replaceAll("-","");
+    var count = 0;
+    console.log(birthDateStr);
+    for(let i=0; i<birthDateStr.length;i++){
+        count += parseInt(birthDateStr[i]);
+    }
+
+    if((count % luckyNumber.value) === 0){
+        displayMsg.innerHTML = "You are lucky!!!";
+
+    }
+    else{
+        displayMsg.innerHTML = "Unlucky :(";
+    }
+}
+
+btnCheck.addEventListener("click", checkLuckyBirthday);
+
